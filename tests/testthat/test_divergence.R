46385d0808daee280650040c3ddefd1d25e88f78
Arthur Gailes <agailes1@gmail.com>
2020-10-27 06:32:39 -0700

    automate divergence tests

--- tests/testthat/test_divergence.R
+++ tests/testthat/test_divergence.R
@@ -1,43 +1,37 @@
-library(testthat)
-library(devtools)
-library(dplyr)
-library(here)
-setwd(here())
-load_all()
-data("bay_race")
-load('tests/testthat/bay_results.Rdata')
-load('tests/testthat/bay_results_sum.Rdata')
+load('bay_results.Rdata')
 
-proper_length <- nrow(bay_race)
 test_that("Default data works",{
+  proper_length <- nrow(bay_race)
   expect_equal(proper_length, 1588)
 })
 
-#create figures from package as it is now
-div_score <- divergence(bay_race$white,
-  bay_race$hispanic,bay_race$asian,bay_race$black, bay_race$all_other)
-divergence_sum <- divergence(bay_race$white,
-  bay_race$hispanic,bay_race$asian,bay_race$black, bay_race$all_other,
-  summed = T)
-divergence_weight <- divergence(bay_race$white, bay_race$hispanic,
-  bay_race$asian,bay_race$black, bay_race$all_other,summed = 'weighted')
-expect_equal(divergence_weight, bay_results$div_weight)
+test_that("Current divergence matches previous divergence",{
+  div_score <- divergence(bay_race$white,
+    bay_race$hispanic,bay_race$asian,bay_race$black, bay_race$all_other)
+  divergence_sum <- divergence(bay_race$white,
+    bay_race$hispanic,bay_race$asian,bay_race$black, bay_race$all_other,
+    summed = T)
+  divergence_weight <- divergence(bay_race$white, bay_race$hispanic,
+    bay_race$asian,bay_race$black, bay_race$all_other,summed = 'weighted')
+  expect_equal(divergence_weight, bay_results$div_weight)
+})
 
-#try incomplete divergence, compatibility with segregation_measures
-divergence_inc_sum <- divergence(bay_race$white,
-  bay_race$hispanic,bay_race$asian,bay_race$black, weights = bay_race$total_pop,
-  summed = T, rowTotals='weights')
-expect_equal(round(divergence_inc_sum, 4), 0.2096)
+test_that("incomplete divergence works",{
+  # maintins compatibility with segregation_measures; segregation report
+  divergence_inc_sum <- divergence(bay_race$white,
+    bay_race$hispanic,bay_race$asian,bay_race$black, weights = bay_race$total_pop,
+    summed = T, rowTotals='weights')
+  expect_equal(round(divergence_inc_sum, 4), 0.2096)
+})
 
-# test to make sure percentages are working
-div_percent <- mutate_at(bay_race, vars(white, black, hispanic, asian, all_other),
-  list(~(./total_pop))) %>%
-  transmute(div_pcts = divergence(white, black, hispanic, asian, all_other, weights = total_pop))
+test_that("divergence works with population percentages",{
+  library(dplyr)
+  div_percent <- mutate(bay_race, across(c(white, black, hispanic, asian, all_other)),
+    list(~(./total_pop))) %>%
+    transmute(div_pcts = divergence(white, black, hispanic, asian, all_other, weights = total_pop))
+
+  #scores
+  expect_equal(round(div_percent$div_pcts,4), round(bay_results$divergence,4))
+})
 
 
-# formatting
-
-expect_equal(proper_length, length(div_score))
-
-#scores
-expect_equal(round(div_percent$div_pcts,4), round(bay_results$divergence,4))
