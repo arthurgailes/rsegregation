a5928598894afbf1d314a9c0f6d3d9001381efe3
Arthur Gailes <agailes1@gmail.com>
2021-03-06 13:36:59 -0800

    convert to test_that

--- tests/testthat/test_single.R
+++ tests/testthat/test_single.R
@@ -1,16 +1,13 @@
-library(testthat)
-library(devtools)
-library(here)
-setwd(here())
-load_all()
-data("bay_race")
-load('tests/testthat/bay_results.Rdata')
-load('tests/testthat/bay_results_sum.Rdata')
 
-#test isolation
-iso_white <- isolation(bay_race$white, bay_race$total_pop, summed = T)
-expect_equal(iso_white, bay_results_sum$white_iso)
+test_that('current results match pre-calculated values',{
+  load('bay_results.Rdata')
+  load('bay_results_sum.Rdata')
+  #test isolation
+  iso_white <- isolation(bay_race$white, bay_race$total_pop, summed = T)
+  expect_equal(iso_white, bay_results_sum$white_iso)
 
-#test lq
-white_lq <- location_quotient(bay_race$white, bay_race$total_pop)
-expect_equal(white_lq, bay_results$white_lq)
+  #test lq
+  white_lq <- location_quotient(bay_race$white, bay_race$total_pop)
+  expect_equal(white_lq, bay_results$white_lq)
+})
+
